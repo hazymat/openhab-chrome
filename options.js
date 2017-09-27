837b8b4f19cd9200620cd88df46602d64f00bb56
Andre Klärner <kandre@ak-online.be>
2017-09-27 20:41:35 +0200

    Provide only a single URL field to the user
    This allows to fully specify the URL used.
    
    Also cleanup the HTML and remove unnecessary attributes, unsuffix
    the HTML IDs and unsuffix the variables stored in localstorage.

--- options.js
+++ options.js
@@ -1,11 +1,8 @@
 // Saves options to localStorage.
 function save_options() {
-	localStorage["server_id"] = $("#serverid").val();
-	localStorage["port_id"] = $("#portid").val();
-	localStorage["home_id"] = $("#homeid").val();
-	localStorage["path_id"] = $("#pathid").val();
-	localStorage["width_id"] = $("#widthid").val();
-	localStorage["height_id"] = $("#heightid").val();
+	localStorage["URL"] = $("#URL").val();
+	localStorage["width"] = $("#width").val();
+	localStorage["height"] = $("#height").val();
 
 	// Update status to let user know options were saved.
 	$("#status").html("Options Saved.");
@@ -16,37 +13,25 @@ function save_options() {
 
 // Restores select box state to saved value from localStorage.
 function restore_options() {
-	var server_id = localStorage["server_id"];
-	var port_id = localStorage["port_id"];
-	var home_id = localStorage["home_id"];
-	var path_id = localStorage["path_id"];
-	var width_id = localStorage["width_id"];
-	var height_id = localStorage["height_id"];
+	var URL = localStorage["URL"];
+	var width = localStorage["width"];
+	var height = localStorage["height"];
 	
-	if(server_id){
-		$("#serverid").val(server_id);
+	if(URL){
+		$("#URL").val(URL);
 	}
-	if(port_id){
-		$("#portid").val(port_id);
+	if(width){
+		$("#width").val(width_id);
 	}
-	if(home_id){
-		$("#homeid").val(home_id);
-	}
-	if(path_id){
-		$("#pathid").val(path_id);
-	}
-	if(width_id){
-		$("#widthid").val(width_id);
-	}
-	if(height_id){
-		$("#heightid").val(height_id);
+	if(height){
+		$("#height").val(height_id);
 	}
 }
 
 $(document).ready(function(){
 	restore_options();
 	
-	$("#saveButton").click(function(){
+	$("#save").click(function(){
 		save_options();
 	});
 });
--- popup.js
+++ popup.js
@@ -1,10 +1,10 @@
-document.addEventListener('DOMContentLoaded', 
-	function applysettings(){
-		document.getElementById('MyFrameContainer').innerHTML= '<iframe style="border:none;" src="http://' + localStorage["server_id"] + ':' + localStorage["port_id"] + localStorage["path_id"] + localStorage["home_id"] + '" width=100% height=100%></iframe>';
-		document.getElementById('bod').style.width=localStorage["width_id"];
-		document.getElementById('bod').style.height=localStorage["height_id"];
-		if (null == localStorage["server_id"]) {
-			document.getElementById('setup').innerHTML = '<p style="padding:20px;background:#ccc;">Don\'t forget to <a href="options.html">Setup</a> the extension!</p>';
-		}
+document.addEventListener('DOMContentLoaded', function (){
+	if (null == localStorage["URL"]) {
+		document.getElementById('setup').innerHTML = '<p style="padding:20px;background:#ccc;">Please <a href="options.html">configure</a> the extension!</p>';
+	} else {
+		document.getElementById('MyFrameContainer').innerHTML=
+			'<iframe style="border:none;" src="' + localStorage["URL"] + '" width=100% height=100%></iframe>';
+		document.getElementById('bod').style.width=localStorage["width"];
+		document.getElementById('bod').style.height=localStorage["height"];
 	}
-)
+})
