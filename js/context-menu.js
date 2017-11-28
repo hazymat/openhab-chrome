99ca59a57afcfe758026b1be066cc3b9e62133a6
Lauritz Leifermann <lleifermann@communicode.de>
2017-11-28 15:19:11 +0100

    refactor context menu to use chrome storage api

---
+++ js/context-menu.js
@@ -0,0 +1,38 @@
+// This script will create the conext menu for the extension.
+chrome.contextMenus.removeAll();
+
+chrome.contextMenus.create({
+  id: "radioLocal",
+  type: "radio",
+  title: "Local URL",
+  contexts: ["browser_action"],
+  onclick: function() {
+    chrome.storage.local.get(null, function(items) {
+      const fetchedValues = Object(items);
+      let values = {
+        active_server_id: fetchedValues.local_server_id
+      };
+      chrome.storage.local.set(values, function() {
+        console.log("Switched to local URL")
+      });
+    });
+  }
+});
+
+chrome.contextMenus.create({
+  id: "radioRemote",
+  type: "radio",
+  title: "Remote URL",
+  contexts: ["browser_action"],
+  onclick: function() {
+    chrome.storage.local.get(null, function(items) {
+      const fetchedValues = Object(items);
+      let values = {
+        active_server_id: fetchedValues.remote_server_id
+      };
+      chrome.storage.local.set(values, function() {
+        console.log("Switched to remote URL");
+      });
+    });
+  }
+});
