96da021a1fbc7cd336820b6593b1f220a1554c17
Lauritz Leifermann <lleifermann@communicode.de>
2017-11-28 15:23:49 +0100

    refactor to use chrome storage api

--- js/popup.js
+++ js/popup.js
@@ -1,10 +1,15 @@
 document.addEventListener('DOMContentLoaded',
   function applysettings() {
-    document.getElementById('MyFrameContainer').innerHTML = '<iframe style="border:none;" src="' + localStorage["active_server_id"] + localStorage["path_id"] + '" width=100% height=100%></iframe>';
-    document.getElementById('bod').style.width = localStorage["width_id"];
-    document.getElementById('bod').style.height = localStorage["height_id"];
-    if (null == localStorage["local_server_id"]) {
-      document.getElementById('setup').innerHTML = '<p style="padding:20px;background:#ccc;">Don\'t forget to <a href="options.html">Setup</a> the extension!</p>';
-    }
+    chrome.storage.local.get(null, function(items) {
+      let fetchedValues = Object(items);
+      if (!fetchedValues.local_server_id && !fetchedValues.remote_server_id) {
+        document.getElementById('setup').innerHTML = '<p style="padding:20px;background:#ccc;">Don\'t forget to <a href="../html/options.html">Setup</a> the extension!</p>';
+      }
+      else{
+        document.getElementById('frameContainer').innerHTML = '<iframe style="border:none;" src="' + fetchedValues.active_server_id + fetchedValues.path_id + '" width=100% height=100%></iframe>';
+        document.getElementById('bod').style.width = fetchedValues.width_id;
+        document.getElementById('bod').style.height = fetchedValues.height_id;
+      }
+    })
   }
 )
